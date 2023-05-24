93fdf7ceae2fa1440f982b5042321325ae7bbb66
azizios011 <hdriaziz699@gmail.com>
2023-05-23 22:10:27 -1100

    sorting algorithgeme header file

--- sort.h
+++ sort.h
@@ -26,4 +26,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/*Helper_Function_ProtoTypes*/
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 #endif
